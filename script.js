b18d186bda49fad7ee6ac553e315dbcada0afa63
Максим <41415269+KemtaTam@users.noreply.github.com>
2021-03-21 14:32:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Ответ: (¬x0 ∧ x1) ∨ (¬x1 ∧ x0)
 const form = document.getElementById("form");
+
 
 answer = {
 	1: ["¬x0∧x1∨¬x1∧x0", "¬x1∧x0∨¬x0∧x1",
@@ -42,6 +42,13 @@ bX1.addEventListener("click", () =>{
 	valOfFunc.focus();
 })
 
+let popup1 = document.getElementsByClassName("pop-up1")[0];
+let popup2 = document.getElementsByClassName("pop-up2")[0];
+let end_button = document.getElementsByClassName("end_button")[0];
+let bOk1 = document.getElementById("bOk1");
+let bOk2 = document.getElementById("bOk2");
+
+//при нажатии на кнопку Отправить ответ
 function retrieveFormValue(event) 
 {
 	event.preventDefault(); //отправлять на сервер не нужно
@@ -53,13 +60,20 @@ function retrieveFormValue(event)
 
 	console.log("v1", value);
 
-	if (checkAnswer(valOfFunc.value)) {
-		alert("Correct answer!");
+	if (checkAnswer(valOfFunc.value)) 
+	{
+		popup1.style.display = "block";
 	}
-	else if (checkForm(form)) alert("Incorrect answer! Try again.");
+	else if (checkForm(form)) 
+	{
+		popup2.style.display = "block";
+	}
 }
 form.addEventListener("submit", retrieveFormValue);
+bOk1.onclick = function(){popup1.style.display = "none";}
+bOk2.onclick = function(){popup2.style.display = "none";}
 
+//проверяет возможные записи правильного ответа
 function checkAnswer(ans)
 {
 	for(var i=0; i<answer[1].length; i++){
@@ -67,7 +81,7 @@ function checkAnswer(ans)
 	}
 	return i != answer[1].length;
 }
-
+//удаляет пробелы и скобки в ответе пользователя
 function delSpace(ans){
 	let newAns = "";
 	for(let i=0; i<ans.length; i++){
@@ -92,3 +106,8 @@ function checkForm(form){
 		return false;
 	} else return true;
 }
+
+
+
+
+
